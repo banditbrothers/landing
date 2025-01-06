df5fa3f5506edef295b50291bad22a89ef6a4b7d
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-01-06 23:50:27 +0530

    fix: correct grammar in WhatsApp order message template

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -5,9 +5,9 @@ export const whatsappKnowMoreLink = `https://api.whatsapp.com/send?phone=9179778
 export const getWhatsappShopNowLink = (name?: string) => {
   let message = "";
   if (name)
-    message = `Hey, I'm interested in ordering the ${name} bandana. Can you help me placing an order?`;
+    message = `Hey, I'm interested in ordering the ${name} bandana. Can you help me place an order?`;
   else
-    message = `Hey, I'm interested in ordering a bandana. Can you help me placing an order?`;
+    message = `Hey, I'm interested in ordering a bandana. Can you help me place an order?`;
 
   const encodedMessage = encodeURIComponent(message);
   return `https://api.whatsapp.com/send?phone=917977884773&text=${encodedMessage}`;
