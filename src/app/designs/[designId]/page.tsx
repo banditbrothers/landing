47aced6d550e22ddd031a5deef2df3269e930b0d
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-01-22 17:34:38 +0530

    test: dynamic images for products

--- src/app/designs/[designId]/page.tsx
+++ src/app/designs/[designId]/page.tsx
@@ -1,5 +1,27 @@
 import { LoadingIcon } from "@/components/misc/loadingScreen";
+
+import { Metadata } from "next";
 import { Suspense } from "react";
+import { designsObject } from "@/data/designs";
+
+export async function generateMetadata({ params }: { params: Promise<{ designId: string }> }): Promise<Metadata> {
+  // read route params
+  const designId = (await params).designId;
+
+  const design = designsObject[designId];
+
+  // fetch data
+  // const product = await fetch(`https://.../${id}`).then(res => res.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: design.name,
+    openGraph: { images: [design.image] },
+    twitter: { images: [design.image] },
+  };
+}
 
 export default function DesignPage() {
   return (
