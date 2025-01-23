b11244c129cac52a11b7ce07b044eec4cfb68179
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-01-24 01:11:44 +0530

    chore: leftover refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { HeroSection } from "@/components/pages/home/hero/HeroSection";
 import { ProductLibrary } from "@/components/pages/home/product-library/ProductLibrarySection";
 import { FeaturesSection } from "@/components/pages/home/features/FeaturesSection";
 import { KnowYourProductSection } from "@/components/pages/home/know-your-product/KnowYourProductSection";
-import { ProductSpecificationsSection } from "@/components/pages/home/product-specs/ProductSpecs";
+import { ProductSpecificationsSection } from "@/components/pages/home/product-specs/ProductSpecsSection";
 import { TestimonialsSection } from "@/components/pages/home/testimonials/TestimonialsSection";
 
 export default function LandingPage() {
