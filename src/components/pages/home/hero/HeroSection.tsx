3015c2267a0d5a3a3b10b3c23c92b4502dfa57e6
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-01-31 00:16:11 +0530

    fix: hero button

--- src/components/pages/home/hero/HeroSection.tsx
+++ src/components/pages/home/hero/HeroSection.tsx
@@ -28,7 +28,7 @@ export const HeroSection = () => {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.4 }}>
-          <Button size="lg" variant="bandit-hover" onClick={() => scrollTo("product-library")}>
+          <Button size="lg" variant="bandit-hover" onClick={() => scrollTo("library")}>
             Be A Bandit
           </Button>
         </motion.div>
