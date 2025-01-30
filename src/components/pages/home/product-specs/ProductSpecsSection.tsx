bcd6e536bd20f109b67529cb4170912f3727ac33
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-01-30 20:36:42 +0530

    chore: update product specs section name

--- src/components/pages/home/product-specs/ProductSpecsSection.tsx
+++ src/components/pages/home/product-specs/ProductSpecsSection.tsx
@@ -2,7 +2,7 @@ import { ProductDetailsAccordion } from "../../../accordions/ProductDetailsAccor
 
 export const ProductSpecificationsSection = () => {
   return (
-    <section id="faq" className="py-20 bg-background/90 text-foreground scroll-mt-16">
+    <section id="product-specs" className="py-20 bg-background/90 text-foreground scroll-mt-16">
       <div className="container mx-auto px-4">
         <h2 className="text-4xl font-bold text-center mb-16">Product Specifications</h2>
 
