c8a8de55e13274e50ffda6ef98644ccbece6e613
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-02-01 15:25:48 +0530

    chore: remove reviews component from product page

--- src/components/pages/designs/index.tsx
+++ src/components/pages/designs/index.tsx
@@ -28,7 +28,6 @@ import { LoadingScreen } from "@/components/misc/Loading";
 import { trackDesignAddToCart, trackDesignView } from "@/utils/analytics";
 import { useCart } from "@/components/stores/cart";
 import { QuantityStepper } from "@/components/misc/QuantityStepper";
-import { Reviews } from "./Reviews";
 
 export const ProductPageContents = ({ designId: paramDesignId }: { designId: string }) => {
   const router = useRouter();
@@ -139,7 +138,6 @@ export const ProductPageContents = ({ designId: paramDesignId }: { designId: str
         </div>
       </div>
 
-      <Reviews currentDesignId={design.id} />
       <RecommendedProducts currentDesignId={design.id} />
     </div>
   );
