2b192020d71f683df4ab63291a63cf4147ca7bac
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-02-02 00:33:46 +0530

    fix: send paymentMethod to discord in rzp webhook

--- src/app/api/rzpWebhook/route.ts
+++ src/app/api/rzpWebhook/route.ts
@@ -31,6 +31,7 @@ export async function POST(request: Request) {
 
         const sendDiscordMessagePromise = new Promise(async resolve => {
           const order = await getOrder(dbId);
+          if (order.paymentMode === "rzp") order.rzp.paymentMethod = paymentMethod;
           await sendDiscordOrderMessage(getDiscordOrderMessage(order));
           resolve(true);
         });
