48e5864862d35771adf80f1c17d641ce09968fdf
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-05-12 00:57:45 +0530

    feat: updated pixel strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${theme}`}>
       <head>
-        <Script id="facebook-pixel" strategy="afterInteractive">
+        <Script id="facebook-pixel" strategy="beforeInteractive">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
