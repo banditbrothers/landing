b0b36e38ed53730c63c1ee2be66ed959ae45af2c
Shamoil Arsiwala <wearebanditbrothers@gmail.com>
2025-06-25 00:38:24 +0530

    feat: cart versioning

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -4,9 +4,23 @@ import { SearchDialog } from "@/components/dialogs/SearchDialog";
 import { LoadingScreen } from "@/components/misc/Loading";
 import { CartSheet } from "@/components/sheets/CartSheet";
 import { useVariants } from "@/hooks/useVariants";
+import { useEffect } from "react";
+
+const LATEST_CART_VERSION = "1";
 
 export default function IndexLayout({ children }: { children: React.ReactNode }) {
   const { data: variants } = useVariants();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const cartVersion = localStorage.getItem("cartVersion");
+      if (!cartVersion || cartVersion !== LATEST_CART_VERSION) {
+        localStorage.setItem("cartVersion", LATEST_CART_VERSION);
+        localStorage.removeItem("cart");
+        window.location.reload();
+      }
+    }
+  }, []);
 
   if (!variants || variants.length === 0) return <LoadingScreen />;
   return (
