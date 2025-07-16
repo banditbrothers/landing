d72000203d394e65bede63a6850810f6a315d1d0
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2025-07-16 14:49:53 +0530

    feat: add size in discord message

--- src/utils/discordMessages.ts
+++ src/utils/discordMessages.ts
@@ -26,7 +26,7 @@ export const getDiscordOrderMessage = (order: Order) => {
           { name: "Address", value: getAddressString(order.address) },
           {
             name: "Products",
-            value: order.variants.map(variant => `${variant.quantity}x ${variant.variantId}`).join("\n"),
+            value: order.variants.map(variant => `${variant.quantity}x ${variant.variantId}-${variant.size}`).join("\n"),
           },
         ],
         timestamp: new Date(getDate(order.createdAt)).toISOString(),
