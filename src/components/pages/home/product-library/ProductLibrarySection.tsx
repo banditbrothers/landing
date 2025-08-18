eaeca4bfda68956188bf87b64aae092840eaad86
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2025-08-18 12:35:31 +0530

    feat: better spacing on product section

--- src/components/pages/home/product-library/ProductLibrarySection.tsx
+++ src/components/pages/home/product-library/ProductLibrarySection.tsx
@@ -20,28 +20,20 @@ export const ProductLibraryContent = () => {
 
   return (
     <>
-      <section id="library-bestsellers" className="scroll-mt-16">
-        <div className=" mx-auto">
-          <div className={`mb-16 flex justify-between items-center max-w-screen-2xl mx-auto px-6 flex-col`}>
+      <section id="library-bestsellers" className="scroll-mt-16 space-y-8">
+        <div className="mx-auto">
+          <div className={`mb-4 flex justify-between items-center max-w-screen-2xl mx-auto px-6 flex-col`}>
             <h2 className="text-4xl font-bold text-center flex flex-row justify-center items-center relative">
               Our Best Sellers
             </h2>
           </div>
           <div className="mx-auto flex flex-col items-center gap-10">
             <ProductCarousel variants={bestSellerVariants} />
-            <Link href="/products">
-              <Button variant="bandit-hover" className="group">
-                <span>View All Products</span>
-                <ArrowRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
-              </Button>
-            </Link>
           </div>
         </div>
-      </section>
-      {unionVariants.length > 0 && (
-        <section id="library-new" className="scroll-mt-16">
+        {unionVariants.length > 0 && (
           <div className=" mx-auto">
-            <div className={`mb-16 flex justify-between items-center max-w-screen-2xl mx-auto px-6 flex-col`}>
+            <div className={`mb-4 flex justify-between items-center max-w-screen-2xl mx-auto px-6 flex-col`}>
               <h2 className="text-4xl font-bold text-center flex flex-row justify-center items-center relative">
                 New Arrivals
               </h2>
@@ -56,8 +48,8 @@ export const ProductLibraryContent = () => {
               </Link>
             </div>
           </div>
-        </section>
-      )}
+        )}
+      </section>
     </>
   );
 };
