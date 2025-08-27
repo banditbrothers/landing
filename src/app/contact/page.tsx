41d98586c14ce47d8f7f165c890b6db1454d8bb7
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2025-08-27 16:13:40 +0530

    feat: add contact, refunds, and shipping policy pages

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+export default function ContactPage() {
+  return (
+    <div className="mt-16">
+      <div className="px-5 py-4 max-w-7xl mx-auto min-h-screen space-y-4">
+        <div className="text-xl font-semibold text-center">Contact Us</div>
+        <div className="flex flex-col gap-4 text-muted-foreground max-w-2xl mx-auto">
+          For any issues in utilizing our services you may contact our helpdesk on
+          <span className="font-semibold">7977884773</span> or
+          <span className="font-semibold">wearebanditbrothers@gmail.com</span>
+        </div>
+      </div>
+    </div>
+  );
+}
